c53ea876dd32fee5ec90d7ec105d35bd84f38824
ming-yi <ming-yi@ming-yideMacBook-Pro.local>
2023-02-01 02:15:23 +0800

    minor change in IMAGE_FITTING_FITS.test.ts

--- src/test/IMAGE_FITTING_FITS.test.ts
+++ src/test/IMAGE_FITTING_FITS.test.ts
@@ -403,7 +403,7 @@ describe("IMAGE_FITTING_FITS test: Testing Image Fitting (with and without fov) 
                         })
                     })
 
-                    let RasterTileDataResponse: Array<CARTA.RasterTileData> = await RasterTileDataPromise;
+                    let RasterTileDataResponse: any = await RasterTileDataPromise;
                     let RasterTileSyncResponse = await RasterTileSyncPromise;
                     let _countFileID999 = 0;
                     let _countFileID998 = 0;
